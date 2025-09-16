96ff1332d7cad9b3bb904e9e6e1374464b8cbbe2
Administrator <Administrator@35-LAB01>
2025-09-16 16:47:16 +0700

    bo sung lan 2

--- src/spc/edu/Bai13_Break.java
+++ src/spc/edu/Bai13_Break.java
@@ -8,9 +8,29 @@ package spc.edu;
  *
  * @author Administrator
  */
-public class Bai13_Break {
-    public static void main(String[] args) {
-        System.out.println("");
+public class Bai13_Break 
+{
+    public static void main(String[] args) 
+    {
+        /*
+        for(int i=1; i<=10000;i++)
+        {
+            if(i==100)
+            {
+                break;
+            }
+            System.out.println(i);
+        }
+        */
         
+        for (int i = 0; i < 10000; i++) 
+        {
+            if (i == 100) 
+            {
+              continue;
+            }
+            System.out.println(i);
+        }
     }
 }
+        
